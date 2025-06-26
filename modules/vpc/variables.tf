cbf275eeaf209aacd97b2026e6c9f40d3ba9886a
shres8 <93535344+shres8@users.noreply.github.com>
2025-06-26 07:03:14 +0530

    Create vpc variables.tf

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,12 @@
+variable "vpc_cidr" {}
+
+variable "project_name" {}
+
+variable region {}
+
+variable pub_sub_1a_cidr {}
+variable pub_sub_2b_cidr {}
+variable pri_sub_3a_cidr {}
+variable pri_sub_4b_cidr {}
+variable pri_sub_5a_cidr {}
+variable pri_sub_6b_cidr {}
